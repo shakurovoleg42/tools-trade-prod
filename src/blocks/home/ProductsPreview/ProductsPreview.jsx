cc36e207cbc079263ee37ac1a0cd3143827dd905
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-08-05 17:18:12 +0500

    slider on main page

--- src/blocks/home/ProductsPreview/ProductsPreview.jsx
+++ src/blocks/home/ProductsPreview/ProductsPreview.jsx
@@ -1,9 +1,11 @@
 "use client";
 import styles from "@/blocks/home/ProductsPreview/ProductsPreview.module.css";
-
 import { useParams } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ProductsPreview = ({ featuredProducts }) => {
   const { region } = useParams();
@@ -18,37 +20,95 @@ const ProductsPreview = ({ featuredProducts }) => {
         <h2 className={styles.PR__title}>Featured products</h2>
       </div>
       <div className={styles.product_container}>
-        {featuredProducts.map((product) => (
-          <div
-            key={product.id}
-            className={styles.poduct_block}
-            onClick={() => handleShowMore(product.slug)}
+        <div className={styles.sliderr}>
+          <Slider
+            className={styles.slyder}
+            dots={true}
+            autoplay={true}
+            infinite={true}
+            slidesToShow={3}
+            slidesToScroll={3}
+            arrows={false}
+            responsive={[
+              {
+                breakpoint: 1200,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 3,
+                  dots: true,
+
+                },
+              },
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  dots: true,
+                },
+              },
+              {
+                breakpoint: 913,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  dots: false,
+                },
+              },
+              {
+                breakpoint: 861,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  dots: false,
+                },
+              },
+              {
+                breakpoint: 350,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                },
+              },
+            ]}
           >
-            <div className={styles.imageContainer}>
-              <Image
-                src={product.image}
-                width={326.38}
-                height={245.14}
-                draggable="false"
-                alt="product"
-              />
-            </div>
-            <div className={styles.title_product}>
-              <p>{product.name}</p>
-            </div>
-            <div className={styles.description}>
-              <div>
-                <b>DESCRIPTION</b>
-                <p>{product.short_description}</p>
+            {featuredProducts.map((product) => (
+              <div
+                key={product.id}
+                className={styles.product_block}
+                onClick={() => handleShowMore(product.slug)}
+              >
+                <div className={styles.imageContainer}>
+                  <Image
+                    src={product.image}
+                    width={326.38}
+                    height={245.14}
+                    draggable="false"
+                    alt="product"
+                    style={{
+                      width: "326.38px !important",
+                      height: "245.14px !important",
+                    }}
+                  />
+                </div>
+                <div className={styles.title_product}>
+                  <p>{product.name}</p>
+                </div>
+                <div className={styles.description}>
+                  <div>
+                    <b>DESCRIPTION</b>
+                    <p>{product.short_description}</p>
+                  </div>
+                </div>
+                <div className={styles.showmore}>
+                  <Link href={`/${region}/products`}>
+                    <button>More</button>
+                  </Link>
+                </div>
               </div>
-            </div>
-            <div className={styles.showmore}>
-              <Link href={`/${region}/products`}>
-                <button>More</button>
-              </Link>
-            </div>
-          </div>
-        ))}
+            ))}
+          </Slider>
+        </div>
       </div>
     </div>
   );
