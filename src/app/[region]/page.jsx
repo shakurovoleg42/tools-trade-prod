2326513dbe96f4bcdb3b22d447f98b4c2ee7a151
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-08-21 11:39:35 +0500

    404 ьфшт

--- src/app/[region]/page.jsx
+++ src/app/[region]/page.jsx
@@ -5,18 +5,26 @@ import FeatureCategories from "@/blocks/home/FeatureCategories/FeatureCategories
 import ProductsPreview from "@/blocks/home/ProductsPreview/ProductsPreview";
 import fetchService from "@/services/fetchs";
 import Head from "next/head";
+import Custom404 from "@/app/[region]/404";
+
 
 export async function generateMetadata({ params }) {
-  const currentRegion = await fetchRegionByCode(params.region);
+  try {
+    const currentRegion = await fetchRegionByCode(params.region);
 
   return {
     title: `Tools & Trade is supplier of Test Instruments in ${currentRegion.name}`,
     description: `Tools & Trade offer best prices for Test Instruments & Industrial Equipment with a wide range of products, FREE delivery in ${currentRegion.name} and delivery to door for all International Orders with duties and taxes included!`,
   };
+  } catch (error) {
+    <Custom404 />;
+  }
+  
 }
 
 const Home = async ({ params }) => {
-  const currentRegion = await fetchRegionByCode(params.region);
+  try {
+    const currentRegion = await fetchRegionByCode(params.region);
   const featuredCategories = await fetchService.getBigCategories();
   const featuredProducts = await fetchService.getFeaturedProducts();
   const region = params.region;
@@ -30,6 +38,10 @@ const Home = async ({ params }) => {
       <ProductsPreview featuredProducts={featuredProducts} />
     </>
   );
+  } catch (error) {
+    <Custom404 />;
+  }
+  
 };
 
 export default Home;
