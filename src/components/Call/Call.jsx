c16dd6af52bfc5b01a7ba6bb17309c00bdabf5d2
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-08-21 10:21:45 +0500

    link to mail chat

--- src/components/Call/Call.jsx
+++ src/components/Call/Call.jsx
@@ -2,20 +2,16 @@
 import Link from "next/link";
 
 const Call = ({ styles }) => {
-  const handleScroll = () => {
-    const section = document.getElementById("quote");
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
   return (
     <div className={styles.toCall}>
-      <button onClick={handleScroll}>Request a quote</button>
-      <Link href='https://api.whatsapp.com/send?phone=971556305217'>
-      <button>Whatsapp us</button>
+      <Link href="mailto:inquiry@gulfinstruments.com">
+        <button>
+          Request a quote
+        </button>
       </Link>
-      
+      <Link href="https://api.whatsapp.com/send?phone=971556305217">
+        <button>Whatsapp us</button>
+      </Link>
     </div>
   );
 };
