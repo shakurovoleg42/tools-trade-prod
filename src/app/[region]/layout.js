a33c961dbf775854d1bdcf1aeedf5f8f6f1da04b
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-09-17 09:14:40 +0500

    hreflangs

--- src/app/[region]/layout.js
+++ src/app/[region]/layout.js
@@ -19,8 +19,29 @@ export default async function RootLayout({ children, params }) {
 
   return (
     <html lang="en">
-      <link rel="alternate" hrefLang='x-default' href={`https://toolsandtrade.com/${params.region}`} />
-      <link rel="alternate" hrefLang={`en-${params.region}`} href={`https://toolsandtrade.com/${params.region}`} />
+      <link rel="alternate" href="https://toolsandtrade.com" hreflang="x-default"/>
+
+<link rel="alternate" href="https://toolsandtrade.com/qa"  hreflang="en-qa"/>
+<link rel="alternate" href="https://toolsandtrade.com/sa"  hreflang="en-sa"/>
+<link rel="alternate" href="https://toolsandtrade.com/kz"  hreflang="en-kz"/>
+<link rel="alternate" href="https://toolsandtrade.com/kw"  hreflang="en-kw"/>
+<link rel="alternate" href="https://toolsandtrade.com/iq"  hreflang="en-iq"/>
+<link rel="alternate" href="https://toolsandtrade.com/eg"  hreflang="en-eg"/>
+<link rel="alternate" href="https://toolsandtrade.com/az"  hreflang="en-az"/>
+<link rel="alternate" href="https://toolsandtrade.com/uz"  hreflang="en-uz"/>
+<link rel="alternate" href="https://toolsandtrade.com/tm"  hreflang="en-tm"/>
+<link rel="alternate" href="https://toolsandtrade.com/mn"  hreflang="en-mn"/>
+<link rel="alternate" href="https://toolsandtrade.com/ru"  hreflang="en-ru"/>
+<link rel="alternate" href="https://toolsandtrade.com/ir"  hreflang="en-ir"/>
+<link rel="alternate" href="https://toolsandtrade.com/cd"  hreflang="en-cd"/>
+<link rel="alternate" href="https://toolsandtrade.com/ao"  hreflang="en-ao"/>
+<link rel="alternate" href="https://toolsandtrade.com/ke"  hreflang="en-ke"/>
+<link rel="alternate" href="https://toolsandtrade.com/gh"  hreflang="en-gh"/>
+<link rel="alternate" href="https://toolsandtrade.com/ng"  hreflang="en-ng"/>
+<link rel="alternate" href="https://toolsandtrade.com/tz"  hreflang="en-tz"/>
+<link rel="alternate" href="https://toolsandtrade.com/pk"  hreflang="en-pk"/>
+<link rel="alternate" href="https://toolsandtrade.com/ae"  hreflang="en-ae"/>
+<link rel="alternate" href="https://toolsandtrade.com/om"  hreflang="en-om"/>
       <body>
         <NextTopLoader />
         <Header logo={main.logo} phone={main.phone} mail={main.mail}/>
