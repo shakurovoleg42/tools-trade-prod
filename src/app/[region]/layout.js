0d21203024df61f9dcf6c5cf38b2d3230826b66e
Oleg Shakurov <thorsonodin2003@gmail.com>
2024-10-01 14:26:42 +0500

    fix google analytics

--- src/app/[region]/layout.js
+++ src/app/[region]/layout.js
@@ -16,26 +16,38 @@ export default async function RootLayout({ children, params }) {
 
   return (
     <html lang="en">
-      <link
-        rel="alternate"
-        href="https://toolsandtrade.com"
-        hreflang="x-default"
-      />
-      <link
-        rel="alternate"
-        href="https://toolsandtrade.com/sa"
-        hreflang="en-sa"
-      />
-      <link
-        rel="alternate"
-        href="https://toolsandtrade.com/ae"
-        hreflang="en-ae"
-      />
+      <head>
+        {/* Google Tag Manager script */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q0VB9YY4E5"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Q0VB9YY4E5');
+          `,
+        }} />
+        <link
+          rel="alternate"
+          href="https://toolsandtrade.com"
+          hreflang="x-default"
+        />
+        <link
+          rel="alternate"
+          href="https://toolsandtrade.com/sa"
+          hreflang="en-sa"
+        />
+        <link
+          rel="alternate"
+          href="https://toolsandtrade.com/ae"
+          hreflang="en-ae"
+        />
+      </head>
       <body>
         <NextTopLoader />
         <Header phone={main.phone} mail={main.mail} />
         <main>{children}</main>
-        <Request email_address={main.email_address} address={main.address}/>
+        <Request email_address={main.email_address} address={main.address} />
         <Footer
           region={params.region}
           phone={main.phone}
